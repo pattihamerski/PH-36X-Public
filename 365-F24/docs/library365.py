77e63085ffe780c08d2a4606888a41bfec609a60
pattihamerski <68252665+pattihamerski@users.noreply.github.com>
2025-01-02 15:10:58 -0800

    Uploading Fall 2024 files and reorganizing

---
+++ 365-F24/docs/library365.py
@@ -0,0 +1,5 @@
+def whats_my_name(name):
+    print("Greetings " + name + ", esteemed majesty.")
+
+monsters = ["vampire", "werewolf", "zombie", "mummy", "ghost", "witch",
+            "frankenstein", "ghoul", "goblin", "troll", "alien"]
